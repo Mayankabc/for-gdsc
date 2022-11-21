8a74ff84be3b9d21dc31ad474980d1b6da9768ba
Mayank Gupta <90678937+Mayankabc@users.noreply.github.com>
2022-11-21 20:16:40 +0530

    Add files via upload

---
+++ desidera project/desidera.js
@@ -0,0 +1,17 @@
+
+        function first(){
+            document.getElementById("slideimage").src="https://images8.alphacoders.com/405/thumbbig-405309.webp";
+        }
+        function second(){
+            document.getElementById("slideimage").src="https://images6.alphacoders.com/346/thumbbig-346068.webp";
+        }
+        function third(){
+            document.getElementById("slideimage").src="https://images5.alphacoders.com/632/thumbbig-632980.webp";
+        }
+        function fourth(){
+            document.getElementById("slideimage").src="https://images7.alphacoders.com/397/thumbbig-397808.webp" ;
+        }
+        setInterval(first,2000);
+        setInterval(second,4000);
+        setInterval(third,6000);
+        setInterval(fourth,8000);
